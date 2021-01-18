06e25f55f5cd97dbc35a06d54889e77f5f6998b3
mush42 <musharraf.omer@gmail.com>
2021-01-19 00:11:58 +0200

    Added .gitignore. Packaging changes.

--- build/lib/accessible_output2/__init__.py
+++
@@ -1,5 +0,0 @@
-# coding: utf-8
-
-from .ao2 import *
-
-__version__ = "0.4"
--- build/lib/accessible_output2/__pyinstaller/__init__.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-
-import os
-
-
-def get_hook_dirs():
-    return os.dirname(os.path.abspath(__file__))
